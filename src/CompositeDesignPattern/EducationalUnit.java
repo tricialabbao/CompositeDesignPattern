684980fb224e4501f4977b833959a278d9c8ddcf
Tricia A. Labbao <tricialabbao.05@gmail.com>
2026-05-01 12:54:14 +0800

    Delete src/CompositeDesignPattern directory

--- src/CompositeDesignPattern/EducationalUnit.java
+++
@@ -1,9 +0,0 @@
-package CompositeDesignPattern;
-
-public interface EducationalUnit {
-
-    void displayDetails(String indent);
-    int getStudentCount();
-    double getBudget();
-    String getName();
-}
